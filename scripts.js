52de4bf3b1caa6625bf8e6a0df6bca202c27f508
Jared Thompkins <jaredthompkins21@gmail.com>
2023-05-10 09:33:23 -0500

    add footer

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function toggleDescription(button) {
+    const action = button.getAttribute('data-action');
+    const moreText = button.parentElement.querySelector('.more');
+    const seeMoreButton = button.parentElement.querySelector('.see-more-btn[data-action="more"]');
+    const seeLessButton = button.parentElement.querySelector('.see-more-btn[data-action="less"]');
+  
+    if (action === 'more') {
+      moreText.style.display = 'inline';
+      seeMoreButton.style.display = 'none';
+      seeLessButton.style.display = 'inline';
+    } else {
+      moreText.style.display = 'none';
+      seeMoreButton.style.display = 'inline';
+      seeLessButton.style.display = 'none';
+    }
+  }
+  
+  
