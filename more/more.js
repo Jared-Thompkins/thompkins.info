99f65ffa1045b4a8161bde84aa3e3d551caeae63
Jared Thompkins <jaredthompkins21@gmail.com>
2023-05-26 15:13:30 -0500

    add zoom defaults

--- more/more.js
+++ more/more.js
@@ -1,3 +1,19 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('show');
+  document.querySelector('.nav-links').classList.toggle('show');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  let initialSize = window.matchMedia("(min-width: 1479px)").matches ? "max" : "fit";
+
+  Fancybox.bind('[data-fancybox="gallery"]', {
+      Images: {
+          initialSize: initialSize,
+          Panzoom: {
+              maxScale: 2,
+          },
+          zoom: true,
+          zoomOpacity: "auto"
+      },
   });
+});
+
